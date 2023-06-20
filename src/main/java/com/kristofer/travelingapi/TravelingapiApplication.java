b55c711f1eb638f6981c7d16c64d5d8f3ef3d8a4
Kristofer Krindges <thecontt06@gmail.com>
2023-06-20 15:49:57 -0300

    initial project

---
+++ src/main/java/com/kristofer/travelingapi/TravelingapiApplication.java
@@ -0,0 +1,13 @@
+package com.kristofer.travelingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelingapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kristofer/travelingapi/TravelingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristofer.travelingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
